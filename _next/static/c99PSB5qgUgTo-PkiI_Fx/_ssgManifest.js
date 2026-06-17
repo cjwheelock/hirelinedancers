5dc94a74798a79abaaee5d2d281b6cb2e381a1b1
CJ Wheelock <cj.wheelock1@gmail.com>
2026-06-17 19:17:01 +0000

    Deploy redesigned site

---
+++ _next/static/c99PSB5qgUgTo-PkiI_Fx/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fcities\u002F[city]","\u002Fevents\u002F[event]","\u002Finstructors\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/chunks/app/instructors/[slug]/page-a15250db4d6e57c1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[975],{8262:(e,s,_)=>{Promise.resolve().then(_.bind(_,171))}},e=>{e.O(0,[730,661,171,441,255,358],()=>e(e.s=8262)),_N_E=e.O()}]);
